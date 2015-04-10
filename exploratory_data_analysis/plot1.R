4cea212332b61f732a014e28fd5ae59ccd11be34
John <jcomo@metrohealth.org>
2015-04-10 13:49:06 -0400

    push plot1 to GitHub repo

---
+++ exploratory_data_analysis/plot1.R
@@ -0,0 +1,8 @@
+data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
+data[,1]<-as.Date(data[,1], format="%d/%m/%Y")
+data[,3]<-as.numeric(as.character(data[,3]))
+data2days<-subset(data, (Date=="2007-02-01")|(Date=="2007-02-02"))
+hist(data2days$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+png(filename = "plot1.png", width=480, height=480)
+## plot(chart)
+dev.off()
