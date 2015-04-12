aa10859d2cbc9f15ce8a8687a7fad2dd22e71242
John <jcomo@metrohealth.org>
2015-04-12 17:27:29 -0400

    saving plot 1 to GitHub

--- plot1.R
+++ plot1.R
@@ -1,10 +1,9 @@
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 
-data[,1]<-as.Date(data[,1], format="%d/%m/%Y")
-data[,3]<-as.numeric(as.character(data[,3]))
-data2days<-subset(data, (Date=="2007-02-01")|(Date=="2007-02-02"))
+data2days<-subset(data, (Date=="1/2/2007")|(Date=="2/2/2007"))
+data2days$Global_active_power<-as.numeric(as.character(data2days$Global_active_power))
 
-with (data2days, hist(data2days$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red"))
+png(filename = "plot1.png", width=480, height=480)
+with(data2days, hist(Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red"))
 
-dev.copy(png, file = "plot1.png", width=480, height=480) ## Copy the histogram to a png file
 dev.off()
