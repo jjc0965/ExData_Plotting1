b6494ae5de7ee5c8f1d7f8d1f57e9729a0524bd8
John <jcomo@metrohealth.org>
2015-04-12 16:50:22 -0400

    saving plot 2 to GitHub

--- plot2.R
+++ plot2.R
@@ -1,14 +1,13 @@
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 
+####################################################################################
+
 data2days<-subset(data, (Date=="1/2/2007")|(Date=="2/2/2007"))
 data2days$Date<-as.Date(data2days$Date, format="%d/%m/%Y")
 data2days$Global_active_power<-as.numeric(as.character(data2days$Global_active_power))
-data2days$Day<-weekdays(as.Date(data2days$Date))
-datetime<-paste(data2days$Date, data2days$Time)
+data2days$Datetime<-as.POSIXct(paste(data2days$Date, data2days$Time))
 
-## data2days<-transform(data2days, Day = factor(Day))
+png(filename = "plot2.png", width=480, height=480)
+with (data2days, plot(Global_active_power~Datetime, ylab="Global Active Power (kilowatts)", xlab="", type="l"))
 
-plot(data2days$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l")
-
-dev.copy(png, file = "plot2.png", width=480, height=480)
 dev.off
