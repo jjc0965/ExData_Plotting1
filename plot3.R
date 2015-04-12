fbb6207b836cbca82b0dd0a20148d581d19df738
John <jcomo@metrohealth.org>
2015-04-12 14:15:39 -0400

    saving plots 3 and 4 to GitHub

--- plot3.R
+++ plot3.R
@@ -1,18 +1,23 @@
 data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 
+#####################################################################################
+
 data2days<-subset(data, (Date=="1/2/2007")|(Date=="2/2/2007"))
 data2days$Date<-as.Date(data2days$Date, format="%d/%m/%Y")
-data2days$Global_active_power<-as.numeric(as.character(data2days$Global_active_power))
 data2days$Day<-weekdays(as.Date(data2days$Date))
 datetime<-paste(data2days$Date, data2days$Time)
 
-with(data2days, {
-  plot(Day, xlab="", ylab="Energy sub metering", ylim=c(0,40))
-  points(Sub_metering_1, col="blue", type="l")
-  points(Sub_metering_2, col="red", type="l")
-  points(Sub_metering_3, col="green", type="l")
-})
-legend("topright", pch="_", col=c("blue", "red", "green"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+data2days$Sub_metering_1<-as.numeric(as.character(data2days$Sub_metering_1))
+data2days$Sub_metering_2<-as.numeric(as.character(data2days$Sub_metering_2))
+data2days$Sub_metering_3<-as.numeric(as.character(data2days$Sub_metering_3))
 
-dev.copy(png, file = "plot3.png", width=480, height=480)
+png(filename = "plot3.png", width=480, height=480)
+with(data2days, {
+  plot(Day, xlab="", ylab="Energy sub metering", ylim=c(0,38))
+  points(Sub_metering_1, col="black", type="l")
+  points(Sub_metering_2, col="red", type="l")
+  points(Sub_metering_3, col="blue", type="l")
+})
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.7)
+
 dev.off
--- plot4.R
+++ plot4.R
@@ -4,28 +4,29 @@ data<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 
 data2days<-subset(data, (Date=="1/2/2007")|(Date=="2/2/2007"))
 data2days$Date<-as.Date(data2days$Date, format="%d/%m/%Y")
+data2days$Day<-weekdays(as.Date(data2days$Date))
+datetime<-paste(data2days$Date, data2days$Time)
+
 data2days$Global_active_power<-as.numeric(as.character(data2days$Global_active_power))
 data2days$Sub_metering_1<-as.numeric(as.character(data2days$Sub_metering_1))
 data2days$Sub_metering_2<-as.numeric(as.character(data2days$Sub_metering_2))
 data2days$Sub_metering_3<-as.numeric(as.character(data2days$Sub_metering_3))
 data2days$Voltage<-as.numeric(as.character(data2days$Voltage))
 data2days$Global_reactive_power<-as.numeric(as.character(data2days$Global_reactive_power))
-data2days$Day<-weekdays(as.Date(data2days$Date))
-datetime<-paste(data2days$Date, data2days$Time)
 
+png(filename = "plot4.png", width=480, height=480)
 par(mfrow = c(2,2))
 with (data2days, {
   plot(Global_active_power, ylab="Global Active Power", xlab="", type="l")
   plot(Voltage, ylab="Voltage", xlab="date/time", type="l")
   with(data2days, {
     plot(Day, xlab="", ylab="Energy sub metering", ylim=c(0,40))
-    points(Sub_metering_1, col="blue", type="l")
+    points(Sub_metering_1, col="black", type="l")
     points(Sub_metering_2, col="red", type="l")
-    points(Sub_metering_3, col="green", type="l")
+    points(Sub_metering_3, col="blue", type="l")
   })
-  legend("topright", pch="_", col=c("blue", "red", "green"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", pch="_", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power, ylab="Global Reactive Power", xlab="date/time", type="l")
 })
 
-dev.copy(png, file = "plot4.png", width=480, height=480)
 dev.off
